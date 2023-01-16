bfc2dca06d093bfcbd794114251a9d4af32dc3b5
Mech <83923848+7heMech@users.noreply.github.com>
2023-01-16 20:40:05 +0000

    Fixed request headers and package structure.

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
-const fetch = require("./fetch.cjs");
+const { SID: CONNECT_SID = "" } = process.env,
+		request = fetch || require("./fetch.cjs");
 /**
  * @param {String} query The graphql query
- * @param {(Object|String)} variables The query variables
- * @returns {Object} Query response
+ * @param {(Object|String)} variables The variables used in the query
+ * @returns {Promise<Object>} Query response
  */
 const graphql = async (query, variables) => {
-	const response = await fetch(`https://replit.com/graphql?e=${Math.round(Math.random() * 100)}`, {
+		const response = await request(`https://replit.com/graphql?e=${Math.round(Math.random() * 100)}`, {
 		"headers": {
+			"User-Agent": "replit-api",
 			"Accept": "application/json",
-			"User-Agent": "replit",
-			"X-Requested-With": "replit",
 			"Origin": "https://replit.com",
+			"Referrer": "https://replit.com",
 			"Content-Type": "application/json",
-			"Referrer-Policy": "strict-origin-when-cross-origin",
-			"Cookie": "connect.sid=" + process.env.SID
+			"X-Requested-With": "XMLHttpRequest",
+			"Cookie": "connect.sid=" + CONNECT_SID
 		},
-		"body": JSON.stringify({ query, variables }),
+		"body": JSON.stringify({query, variables}),
 		"method": "POST"
 	});
 	const res = await response.json();
